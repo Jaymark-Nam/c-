1d31af26a593082ea12bc387356693d39133bc40
Jaewoo Nam <hamellnam@gmail.com>
2020-04-08 23:24:04 +0900

    2020_04_08

---
+++ firststep.cpp
@@ -0,0 +1,15 @@
+int main() 
+{
+  
+  std::cout << "Hello World!\n";
+  
+}
+
+compiler
+g++ hello.cpp
+./a.out
+
+To compile a file, you need to type g++followed by file name in the terminal
+The compiler will translate the c++ program hello.cpp and create a machine code file called a.out
+
+To execute the new machine code file, all you need to do is type ./ and the file name in the terminal
