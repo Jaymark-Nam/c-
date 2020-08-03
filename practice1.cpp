d58b497642e521fd542f86c53cb4cee7ed8111a5
Jaewoo Nam <hamellnam@gmail.com>
2020-08-03 12:36:42 +0900

    2020.08.03

---
+++ practice1.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main()
+{
+ std::cout << "insert money" << std::endl;
+ int cal_tot, cal_point;  
+ std::cin >> cal_tot;
+ cal_point = cal_tot * 0.01;
+ std::cout << "Point is" <<cal_point << "Won.\n";
+
+}
